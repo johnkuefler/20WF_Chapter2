cb16f413a667c513d4535c4430b8115406c3a5d1
John Kuefler <john@dev-squared.com>
2020-08-27 11:48:36 -0500

    Added some samples

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,4 +10,28 @@
  */
 public class Main {
     
+    public static void main(String[] args) {
+        System.out.println("Hello \"world\"");
+        
+        String myHelloWorldString = "Hello again";
+        
+        int myNumberFive = 5;
+        int myNumberSix =6;
+        int myNumberSeven = 7;
+                
+        System.out.println(myNumberFive);
+        
+        double myDouble = 5.4;
+        boolean myBool = false;
+        char jChar = 'j';
+        char JChar = 'J';
+        String mChar = "M";
+        
+        System.out.println("This is some big"
+                + " long string of text"
+                + " that will run way"
+                + " off the screen");
+        
+        System.out.println(55);
+    }
 }
